dff4a1b20421d880c7d455600011d367f7536f6a
godtanatos <78075671+godtanatos@users.noreply.github.com>
2021-08-17 13:12:49 +0300

    feat: ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brakets = {};
+  bracketsConfig.forEach(elem => { brakets[elem[1]] = elem[0] })
+  let bracketsRight = Object.keys(brakets);
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+    if (isCloseBracket(current)) {
+      if (brakets[current] == stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+
+        return false;
+      }
+
+    } else {
+
+      stack.push(current);
+    }
+
+  }
+
+  return stack.length === 0;
+  function isCloseBracket(ch) {
+    if (brakets[ch] == ch) {
+      brakets[ch] = ""
+      return false
+    } else {
+      if (brakets[ch] == "") {
+        brakets[ch] = ch
+      }
+      return bracketsRight.indexOf(ch) > -1
+    }
+
+  }
+
 }
